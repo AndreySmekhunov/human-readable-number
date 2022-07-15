0a08a159ca6f07a32f9b5036fb8fee6608a98602
AndreySmekhunov <smekhunov@gmail.com>
2022-07-15 11:34:06 +0300

    feat: fix error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+
+        let l = number.toString().length;
+        let str = "";
+        let arrstr = number.toString().split('').reverse(); 
+        let arr = [0,0,0,0,0,0];
+        for (let i = 0; i < l; i++)
+          arr[i] = parseInt(arrstr[i]);
+          console.log(arr);
+        let arr19 = ["","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+        let arr2090 = ["","","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]  
+          if (number < 1) return "zero";
+          if (arr[1] < 2) str = arr19[arr[0] + arr[1]*10]
+            else str = arr2090[arr[1]] + " " + arr19[arr[0]];
+        str = str.trim();
+          
+        if (number < 100) return str
+          else return (arr19[arr[2]] + " hundred " + str).trim();
+     
 }
